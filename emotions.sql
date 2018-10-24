6c77be1be662e2669ac4d1ac46ee1ff0cf879383
Ondřej Kužel <ondrej.kuzel@gmail.com>
2018-10-24 13:16:27 +0200

    DB coreponds to sheet

--- emotions.sql
+++ emotions.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.8
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
 -- Počítač: localhost
--- Vytvořeno: Ned 30. zář 2018, 09:20
--- Verze serveru: 10.1.35-MariaDB
--- Verze PHP: 5.6.38
+-- Vytvořeno: Stř 24. říj 2018, 13:13
+-- Verze serveru: 5.7.22-0ubuntu0.16.04.1
+-- Verze PHP: 7.0.30-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -69,7 +67,9 @@ INSERT INTO `odpoved` (`id_problemu`, `id_kog_schematu`, `odpoved`, `smazano`) V
 (2, 1, 'ZtrÃ¡ta zamÄ›stnÃ¡nÃ­ je velmi Å¡patnÃ¡ a mÃ¡ spoustu negativnÃ­ch dÅ¯sledkÅ¯', 0),
 (2, 2, 'MÅ¯Å¾e to bÃ½t dobrÃ© k tomu, Å¾e ÄlovÄ›k bude mÃ­t nÄ›jakou dobu vÃ­ce Äasu, a pak tÅ™eba poznÃ¡ zase jinÃ© zamÄ›stnÃ¡nÃ­.', 0),
 (2, 3, 'ZtrÃ¡ta zamÄ›stnÃ¡nÃ­ ani Å¾Ã¡dnÃ½ jejÃ­ dÅ¯sledek ve skuteÄnosti nebrÃ¡nÃ­ tomu, aby byl ÄlovÄ›k spokojen.', 0),
-(2, 4, 'ZtrÃ¡ta zamÄ›stnÃ¡nÃ­ je moÅ¾nÃ¡ nÄ›jakÃ¡ vyÅ¡Å¡Ã­ vÅ¯le a nemÃ¡ cenu se s tÃ­m nÄ›jak trÃ¡pit.', 0);
+(2, 4, 'ZtrÃ¡ta zamÄ›stnÃ¡nÃ­ je moÅ¾nÃ¡ nÄ›jakÃ¡ vyÅ¡Å¡Ã­ vÅ¯le a nemÃ¡ cenu se s tÃ­m nÄ›jak trÃ¡pit.', 0),
+(3, 1, 'Je to nanic, spoustu vÄ›cÃ­ musÃ­m odloÅ¾it a cÃ­tÃ­m se zle.', 0),
+(3, 2, 'OnemocnÄ›t Äas od Äasu zaktivuje imunitnÃ­ systÃ©m', 0);
 
 -- --------------------------------------------------------
 
@@ -92,7 +92,15 @@ INSERT INTO `problem` (`id`, `nazev`, `popis`, `smazano`) VALUES
 (1, 'VÃ½smÄ›ch okolnÃ­ch lidÃ­', '', 0),
 (2, 'ZtrÃ¡ta zamÄ›stnÃ¡nÃ­', '', 0),
 (3, 'MÃ¡m chÅ™ipku', '', 0),
-(4, 'Nic nestÃ­hÃ¡m, jde mi vÅ¡echno straÅ¡nÄ› pomalu', '', 0);
+(4, 'Nic nestÃ­hÃ¡m, jde mi vÅ¡echno straÅ¡nÄ› pomalu', '', 0),
+(6, 'ZtrÃ¡ta partnera', '', 0),
+(7, 'ProblÃ©my v rodinÄ›', '', 0),
+(8, 'ZtrÃ¡ta penÄ›Å¾enky', '', 0),
+(9, 'Å patnÃ© investice penÄ›z', '', 0),
+(10, 'Nerozhodnost', '', 0),
+(11, 'Hlasy', '', 0),
+(12, 'Å patnÃ½ spÃ¡nek', '', 0),
+(13, 'Å patnÃ© vstÃ¡vÃ¡nÃ­', '', 0);
 
 --
 -- Klíče pro exportované tabulky
@@ -125,14 +133,11 @@ ALTER TABLE `problem`
 --
 ALTER TABLE `kognitivni_schema`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
 --
 -- AUTO_INCREMENT pro tabulku `problem`
 --
 ALTER TABLE `problem`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
