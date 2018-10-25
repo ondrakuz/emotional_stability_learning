49884be7c35770099608331f216f1832bb395cee
ondrakuz <ondrej.kuzel@gmail.com>
2018-10-25 12:38:04 +0200

    Debug: HomePage

--- www/administrace/controllers/HomePageController.php
+++ www/administrace/controllers/HomePageController.php
@@ -1,14 +1,12 @@
 <?php
 class HomePageController extends Controller
 {
-    public function setView()
-    {
-		// Hlavička požadavku
-		header("Hlavní stránka");
-		// Hlavička stránky
-		$this->header['title'] = 'Hlavní stránka';
-		// Nastavení šablony
-		$this->view = 'homePage';
-    }
+  public function setView()
+  {
+  // Hlavička stránky
+  $this->header['title'] = 'Hlavní stránka';
+  // Nastavení šablony
+  $this->view = 'homePage';
+  }
 }
 ?>
