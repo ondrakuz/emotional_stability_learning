2a77c4b56aa7a51a47dc77a3fd39ae3fa8e10e72
Ondřej Kužel <ondrej.kuzel@gmail.com>
2019-02-07 10:01:15 +0100

    wrong answer checkbox in administration

--- www/administrator/controllers/CSchemaAddController.php
+++ www/administrator/controllers/CSchemaAddController.php
@@ -4,7 +4,9 @@ class CSchemaAddController extends Controller
   public function ctrMain($parameters)
   {
     $cschemaModel = new CSchemaModel();
-    if ($cschemaModel->insert(array('name' => "'".htmlspecialchars($_POST['name'], ENT_QUOTES)."'")))
+    
+    if ($_POST['wrong']) { $wrong = 1; } else { $wrong = 0; }
+    if ($cschemaModel->insert(array('name' => "'".htmlspecialchars($_POST['name'], ENT_QUOTES)."'", 'id_lang' => $_POST['lang'], 'wrong' => $wrong)))
     {
       $this->redirect('/administrator/c-schema-overview');
     }
--- www/administrator/controllers/CSchemaUpdateController.php
+++ www/administrator/controllers/CSchemaUpdateController.php
@@ -5,7 +5,8 @@ class CSchemaUpdateController extends Controller
   {
     $cschemaModel = new CSchemaModel();
     
-    if ($cschemaModel->update(htmlspecialchars($_POST['id'], ENT_QUOTES), array('name' => "'".htmlspecialchars($_POST['name'], ENT_QUOTES)."'", 'id_lang' => $_POST['lang'])))
+    if ($_POST['wrong']) { $wrong = 1; } else { $wrong = 0; }
+    if ($cschemaModel->update(htmlspecialchars($_POST['id'], ENT_QUOTES), array('name' => "'".htmlspecialchars($_POST['name'], ENT_QUOTES)."'", 'id_lang' => $_POST['lang'], 'wrong' => $wrong)))
     {
       $this->redirect('/administrator/c-schema-overview');
     }
