30612c95ae54285746443138eedc561fc3fe45fb
Ondřej Kužel <ondrej.kuzel@gmail.com>
2019-02-11 21:33:02 +0100

    "No users" text in fUsersOverview.phtml - debug

--- www/administrator/controllers/FusersOverviewController.php
+++ www/administrator/controllers/FusersOverviewController.php
@@ -4,14 +4,14 @@ class FusersOverviewController extends Controller
     public function ctrMain($parameters)
     {
         $fusersModel = new FUsersModel();
-        if ($fusers = $fusersModel->selectAll())
-        {
-            $this->headr['title'] = "Studenti a Lektoři";
-            
-            $this->data['fusers'] = $fusers;
-            
-            $this->view = 'fUsersOverview';
-        }
+        
+        $fusers = $fusersModel->selectAll();
+        
+        $this->headr['title'] = "Studenti a Lektoři";
+        
+        $this->data['fusers'] = $fusers;
+        
+        $this->view = 'fUsersOverview';
     }
 }
 ?>
